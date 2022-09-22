456de178742a29993ff36db9cb60f15fc1f9156d
Jay Kumar <jaytechbc@gmail.com>
2022-09-22 11:35:04 +0530

    Initial commit

---
+++ app/src/main/java/com/github/jay/filepicker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.jay.filepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toaster.show(this,"HI");
+    }
+}
